b52e5a29f4b93435b061d4c00ab085a053b29e27
aliff.macapinlac <aliff.macapinlac@gmail.com>
2017-03-28 11:03:59 -0700

    layout formatting on commentwall

--- public/js/myJS.js
+++ public/js/myJS.js
@@ -15,11 +15,10 @@
 
 
   		$.get('/commentWall', function(data){
-  		var html = 	"{{#each .}}<h1>User: {{name}}</h1>"+
-		"<h2>Subject: {{subject}}</h2>"+
-		"<p>Comment: {{commentString}}</p>{{/each}}";
+  		var html = 	"{{#each .}}<div class='row' style='margin-left: 10px;'><img class='img-responsive' width=100 src='images/svg/commentHead.svg' style='float:left; margin-right: 15px;'/> <h4>  User: {{name}}</h4>"+
+		"<h5>Subject: {{subject}}</h5></div>"+
+		"<div class='well' style='margin-top: 10px; margin-left: 10px;'><p>Comment: {{commentString}}</p></div></div>{{/each}}";
   		
-
 	  	var template = Handlebars.compile(html);
 
 	  	var stuff = template(data);
